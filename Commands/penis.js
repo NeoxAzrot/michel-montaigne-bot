d7deeb62e383645696cc83f0b962da93a995541d
NeoxAzrot1 <sami.lepays@gmail.com>
2020-11-30 18:42:23 +0100

    Penis mention

--- Commands/penis.js
+++ Commands/penis.js
@@ -2,11 +2,23 @@ const Discord = require('discord.js');
 
 module.exports.run = async(client, message, args) => {
 
-  const penis_size = penis_random()
-  let penis = ""
+  let penis_size = penis_random();
+  let penis = "";
 
   for (let i = 0; i < penis_size; i++) {
-    penis += "="
+    penis += "=";
+  }
+
+  let memberpenis = message.mentions.users.first();
+  let name;
+  let icon_url;
+  
+  if (!memberpenis){
+      name = message.author.username;
+      icon_url = message.author.avatarURL;
+  } else {
+      name = memberpenis.username;
+      icon_url = memberpenis.avatarURL;
   }
 
   return message.channel.send({
@@ -14,8 +26,8 @@ module.exports.run = async(client, message, args) => {
           color: parseInt('0x' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6)),
           description: `8${penis}D`,
           author: {
-              name: `Penis de ${message.author.username}`,
-              icon_url: message.author.avatarURL,
+              name: `Penis de ${name}`,
+              icon_url: icon_url,
               proxy_icon_url: ' '
             }
       }
@@ -25,7 +37,7 @@ module.exports.run = async(client, message, args) => {
 
 function penis_random(min, max) {
     min = Math.ceil(0);
-    max = Math.floor(55);
+    max = Math.floor(50); // 55 max screen
     return randnum = Math.floor(Math.random() * (max - min +1) + min);
 }
 
@@ -34,5 +46,5 @@ module.exports.help = {
     category: ":circus_tent: | Fun",
     name: "penis",
     description: "Affiche une taille de penis aléatoire",
-    usage: "/penis"
+    usage: "/penis ou /penis @mention"
 }
